0c1e4b77b48292571d757cb7685f181f465250d8
LudoPilot <e.berardfr@gmail.com>
2023-12-29 10:09:40 +0100

    First commit

---
+++ back-end/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+	use Doctrine\ORM\Tools\Setup;
+	use Doctrine\ORM\EntityManager;
+	date_default_timezone_set('America/Lima');
+	require_once "vendor/autoload.php";
+	$isDevMode = true;
+	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+	$conn = array(
+	'host' => 'dpg-cm23hsa1hbls73bu4ai0-a.frankfurt-postgres.render.com',
+
+	'driver' => 'pdo_pgsql',
+	'user' => 'projet_angular_bdd_user',
+	'password' => '1YHzChp7dvc1a5yUZugO7cnpuVXS8fdw',
+	'dbname' => 'cnam_db_akkt',
+	'port' => '5432'
+	);
+
+
+	$entityManager = EntityManager::create($conn, $config);
+
+
+
